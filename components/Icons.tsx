42cd3d290fd5110b4412b520ab62463a77d0ab68
rgronow <53609147+rgronow@users.noreply.github.com>
2026-02-13 19:44:13 +0000

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -47,3 +47,9 @@ export const LinkIcon = ({ className }: { className?: string }) => (
      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
   </svg>
 );
+
+export const DownloadIcon = ({ className }: { className?: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M7.5 10.5l4.5 4.5m0 0l4.5-4.5m-4.5 4.5V3" />
+  </svg>
+);
